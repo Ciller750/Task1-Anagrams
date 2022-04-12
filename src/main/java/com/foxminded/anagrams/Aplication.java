add4fc786879d400e77b14ea1526be96a1ea3149
Ciller750 <mszhena@mail.ru>
2022-04-12 13:39:57 +0300

    Built the project structure

---
+++ src/main/java/com/foxminded/anagrams/Aplication.java
@@ -0,0 +1,6 @@
+package com.foxminded.anagrams;
+/*
+The program reads the line and reverses and reverses the latin letters.
+ */
+public class Aplication {
+}
---
+++ src/test/java/com/foxminded/anagrams/AplicationTest.java
@@ -0,0 +1,7 @@
+package com.foxminded.anagrams;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AplicationTest {
+
+}
